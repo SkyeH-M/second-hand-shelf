d88d5b0af5b9317fe511a5d8b5afed1e61e327e0
SkyeH-M <justskye98@gmail.com>
2023-05-25 15:36:10 +0000

    Added plus and minus buttons for book_details.html, need to add JS to make these functional

--- bag/views.py
+++ bag/views.py
@@ -12,12 +12,24 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    quality = None
+    if 'book_quality' in request.POST:
+        quality = request.POST['book_quality']
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
-    else:
-        bag[item_id] = quantity
+    if quality:
+        if item_id in list(bag.keys()):
+            if quality in bag[item_id]['items_by_quality'].keys():
+                bag[item_id]['items_by_quality'][quality] += quantity
+            else:
+                bag[item_id]['items_by_quality'][quality] = quantity
+        else:
+            bag[item_id] = {'items_by_quality': {quality: quantity}}
+    else: 
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
