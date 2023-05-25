bbda7ec2fd70239cd598a5889dea52379eb1a6d9
SkyeH-M <justskye98@gmail.com>
2023-05-25 11:52:30 +0000

    added back to top button and Javascript to sort books

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category
 
 
@@ -19,8 +20,10 @@ def all_books(request):
             sort = sortkey
             if sortkey == 'name':
                 sortkey = 'lower_name'
-                products = products.annotate(lower_name=Lower('name'))
-
+                # does below need to be title, or name?
+                products = products.annotate(lower_name=Lower('title')) 
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
