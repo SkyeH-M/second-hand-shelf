a93a0c439b7d86d3c882f866a7e58251e1339678
SkyeH-M <justskye98@gmail.com>
2023-05-27 14:45:18 +0000

    Begin adding and styling Stripe

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51N7yRFDNZYfoGn9uTTwxhhFPIssmihRgvwg7GUalUjbjjmRpMreT6chmBh52Uw4BKrdkXNV2e3TLe1QAhtbSLJt700atFRELyv'
     }
 
     return render(request, template, context)
