030ed61e1295d76bb4fcac672860673e056e034b
SkyeH-M <justskye98@gmail.com>
2023-05-28 12:52:44 +0000

    Created checkout_success.html, need to populate it

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -175,6 +177,7 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
+DEBUG = "DEVELOPMENT" in os.environ
 
 
 # Default primary key field type
