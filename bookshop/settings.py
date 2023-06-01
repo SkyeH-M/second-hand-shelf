e65cc6d356a4ae664d193f9bd7623197f4c11532
SkyeH-M <justskye98@gmail.com>
2023-06-01 15:21:09 +0000

    Establish confirmation emails

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -179,6 +179,7 @@ STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'secondhandshelf@example.com'
 DEBUG = "DEVELOPMENT" in os.environ
 
 
