f6bd7b846dcdac1188e7fc82f18b9c4ef3fa738b
SkyeH-M <justskye98@gmail.com>
2023-06-05 14:58:58 +0000

    Need to adjust add_to_wishlist_btn for larger screen sizes

--- profiles/models.py
+++ profiles/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import AbstractBaseUser
 from django_countries.fields import CountryField
 
 
-class UserProfile(models.Model): # removed models.Model
+class UserProfile(models.Model):
     """
     A user profile model for maintaining default
     delivery information and order history
@@ -20,8 +20,6 @@ class UserProfile(models.Model): # removed models.Model
     default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
     default_country = CountryField(blank_label='Country', null=True, blank=True)
-
-    # USERNAME_FIELD = ('user')
 
     def __str__(self):
         return self.user.username
