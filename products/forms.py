a1330e4b23175321f19155fd84b2e96280fbe73f
SkyeH-M <justskye98@gmail.com>
2023-06-30 14:25:41 +0000

    Finished book review section of readme features

--- products/forms.py
+++ products/forms.py
@@ -26,10 +26,3 @@ class BookReviewForm(forms.ModelForm):
     class Meta:
         model = BookReview
         fields = ('content', 'stars',)
-
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-
-    #     for field_name, field in self.fields.items():
-    #         field.widget.attrs['class'] = 'review-form-input'
-        # self.fields['body'].widget.attrs = {'rows': 5}
